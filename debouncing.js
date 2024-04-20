d573f97c06e10c4c86c42a237b10f6a17386ac08
Sonu Kumar <sksonu8544@gmail.com>
2024-04-20 09:56:59 +0530

    debouncing in js

---
+++ debouncing.js
@@ -0,0 +1,16 @@
+let count = 0;
+function functionCaling() {
+    console.log("Function Calling here", count++)
+}
+
+function deBounceFunction(data, time) {
+    let newTime;
+    return function (...argss) {
+        if (time) clearTimeout(time);
+        newTime = setTimeout(() => {
+            data.call( argss);
+        }, time)
+    }
+}
+
+const newFunctionCall = deBounceFunction(functionCaling, 500)
