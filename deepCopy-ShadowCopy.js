355ad2ee451c4f3ec1b65dcfd1b1a689bf4e2a24
Sonu Kumar <sksonu8544@gmail.com>
2024-04-28 18:47:33 +0530

    learn deep copy and shallow copy

---
+++ deepCopy-ShadowCopy.js
@@ -0,0 +1,22 @@
+// let userName1 = "sonu";
+// let userName2 = userName1 + "Singh";
+
+
+// const user1 = {
+//     firstName: "sonu",
+//     lastName: "kumar"
+// }
+
+// // const user2 = {};
+// // Object.assign(user2, user1);
+
+// // user2.lastName = "satyam"
+
+// const user2 = {...user1};
+// user2.lastName = "SINGH";
+
+
+let arr1 = [1,2,3,4];
+let arr2 = [...arr1]
+
+arr2.push(32)
